74982d3d27e3705b6b9d25ce735d8e3f3141e9fc
katerinafaustenova <katerina.faustenova@seznam.cz>
2022-11-21 23:05:56 +0800

    initial graphql fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { gql, request } from "graphql-request";
+import { useQuery } from "react-query";
 import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
+
+const endpoint =
+  "https://api-eu-west-2.hygraph.com/v2/claqvecol6m0o01t7fp787wjw/master";
+const postQuery = gql`
+  {
+    posts {
+      id
+      title
+      slug
+      date
+      content {
+        html
+      }
+    }
+  }
+`;
 
 function App() {
+  const { data, isLoading, error } = useQuery("posts", () => {
+    return request(endpoint, postQuery);
+  });
+  console.log("data", data, "loading", isLoading, "err", error);
   return (
     <>
       <Header />
-      <Navbar />
+      {/* <Navbar /> */}
+      <main></main>
     </>
   );
 }
