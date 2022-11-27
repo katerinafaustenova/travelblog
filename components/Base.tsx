0fe79fe83cbd5ee50f5c76c15e349f903bd3bf7a
katerinafaustenova <katerina.faustenova@seznam.cz>
2022-11-27 08:30:40 +0800

    back icon header post detail page

--- components/Base.tsx
+++ components/Base.tsx
@@ -2,10 +2,14 @@ import { getYear } from "date-fns";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import Home from "../assets/home.svg";
 import Logo from "../assets/travel.png";
 import styles from "../styles/Base.module.css";
 
 export default function Base({ children }: any) {
+  const { asPath } = useRouter();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,17 +21,25 @@ export default function Base({ children }: any) {
         <link rel="icon" href="/travel.ico" />
       </Head>
 
-      <header className={styles.header}>
-        <div className={styles.image}>
+      {asPath === "/" ? (
+        <header className={styles.header}>
+          <div className={styles.image}>
+            <Link href="/">
+              <Image src={Logo} alt="Logo" width={80} height={80} />
+            </Link>
+          </div>
+          <div className={styles.title}>
+            <h1>Káťa a Kuba na cestách</h1>
+            <p>Blog o cestování a digitálním nomádství</p>
+          </div>
+        </header>
+      ) : (
+        <header className={styles.home}>
           <Link href="/">
-            <Image src={Logo} alt="Logo" width={80} height={80} />
+            <Image src={Home} alt="Home" width={20} height={20} />
           </Link>
-        </div>
-        <div className={styles.title}>
-          <h1>Káťa a Kuba na cestách</h1>
-          <p>Blog o cestování a digitálním nomádství</p>
-        </div>
-      </header>
+        </header>
+      )}
 
       {children}
 
